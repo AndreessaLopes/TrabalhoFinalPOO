bac07f2ef0d98239a82bdb3ad28b596d67610385
AndreessaLopes <andressacaroline082011@gmail.com>
2023-06-22 16:07:57 -0300

    updates

---
+++ pages/avatar/index.vue
@@ -0,0 +1,9 @@
+<template v-slot:prepend>
+<v-img
+    class="mx-auto"
+    :width="600"
+    aspect-ratio="16/9"
+    cover
+    src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+    ></v-img>
+</template>
