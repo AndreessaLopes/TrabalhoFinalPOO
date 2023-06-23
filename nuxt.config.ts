a45f266fc145f3700302b28e9b557248f445e67c
Bruno <brunorconte@hotmail.com>
2023-06-23 13:08:21 -0300

    enabled dev-tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,37 @@
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
-    runtimeConfig: {
-      public: {
-        apiBase: '',
-      }
+  runtimeConfig: {
+    public: {
+      apiBase: '',
+    }
+  },
+
+  modules: [
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(
+        vuetify()
+      ))
     },
-    modules: [
-      async (options, nuxt) => {
-        nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(
-          vuetify()
-        ))
+  ],
+
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@/assets/global.scss',
+    '@mdi/font/css/materialdesignicons.min.css'
+  ],
+
+  build: {
+      transpile: ['vuetify'],
+  },
+
+  vite: {
+      define: {
+          'process.env.DEBUG': false,
       },
-    ],
-    css: [
-      'vuetify/lib/styles/main.sass',
-      '@/assets/global.scss',
-      '@mdi/font/css/materialdesignicons.min.css'
-    ],
-    build: {
-        transpile: ['vuetify'],
-    },
-    vite: {
-        define: {
-            'process.env.DEBUG': false,
-        },
-    },
+  },
+
+  devtools: {
+    enabled: true,
+  },
 })
